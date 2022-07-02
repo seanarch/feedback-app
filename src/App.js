343ab7c584e0c716ffb2314587d4e9a84372b230
Sean <seanzhu@Seans-Mac-mini.local>
2022-07-02 15:49:50 -0600

    test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "./components/Header"
+import FeedbackItem from "./components/FeedbackItem"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackItem />
+      </div>
+    </>
+  )
+}
+
+export default App
