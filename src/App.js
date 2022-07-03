3feffce01cf3f7d5069b0fa6f75f524717be9116
Sean <seanzhu@Seans-Mac-mini.local>
2022-07-02 21:29:21 -0600

    Update delete feedback function

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import React from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
+import Card from "./components/shared/Card"
+import FeedbackItem from "./components/FeedbackItem"
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback}/>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+         
       </div>
     </>
   )
