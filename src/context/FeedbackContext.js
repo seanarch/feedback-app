2011893c52c886306716d50f41389d87c2ce0502
Sean <seanzhu@Seans-Mac-mini.local>
2022-07-07 23:16:16 -0600

    update and delete feedback

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -23,14 +23,25 @@ export const FeedbackProvider = ({children}) => {
         setIsLoading(false)
     }
 
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if(window.confirm('Are you sure you want to delete?')) {
+            await fetch(`/feedback/${id}`, { method: 'DELETE'})
           setFeedback(feedback.filter((item) => item.id !== id))
         }
       }
 
-    const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map((item)=> item.id === id ? {...item, ...updItem}: item))
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updItem)
+        })
+
+        const data = await response.json() 
+
+        setFeedback(feedback.map((item)=> item.id === id ? {...item, ...data}: item))
     }
 
     const addFeedback = async (newFeedback) => {
